0961d37adf2d1dd72c9743cc3ff89b17394de25f
sivlvy <vladislavsidenko@ukr.net>
2024-05-29 17:15:23 +0300

    upd

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<BrowserRouter basename='/CamperFree'>
+		<BrowserRouter basename="/CamperFree">
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
