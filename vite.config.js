631e324fed4073f483942424cda70d5902fdd44b
sivlvy <vladislavsidenko@ukr.net>
2024-05-29 17:14:22 +0300

    upd

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            src: '/src',
-            components: '/src/components',
-            pages: '/src/pages',
-            assets: '/src/assets',
-        },
-    },
-    base: '/vite-react-eslint-starter-template',
+	plugins: [react()],
+	resolve: {
+		alias: {
+			src: '/src',
+			components: '/src/components',
+			pages: '/src/pages',
+			assets: '/src/assets',
+		},
+	},
+	base: '/CamperFree',
 });
